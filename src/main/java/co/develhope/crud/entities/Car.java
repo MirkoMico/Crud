c31088936358457d4370e2335a673bb4f011093f
MirkoMico <mirkomicoccigmail.com>
2023-03-26 19:47:49 +0200

    first commit

--- src/main/java/co/develhope/crud/entities/Car.java
+++ src/main/java/co/develhope/crud/entities/Car.java
@@ -9,9 +9,6 @@ import javax.persistence.*;
 
 @Entity
 @Table
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Car {
 
@@ -21,4 +18,37 @@ public class Car {
 
     private String modelName;
     private String type;
+
+    public Car() {
+    }
+
+    public Car(long id, String modelName, String type) {
+        this.id = id;
+        this.modelName = modelName;
+        this.type = type;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 }
